5101e6d4ec543cb70f8b0991b49ac112a184f867
huangbinjie <huangbinjie@huangbinjiedeMacBook-Pro.local>
2018-10-26 16:32:04 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, "hello world!")
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	r.Run(":5000")
+}
